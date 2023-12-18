4dcac0cb24620469c43153e1dfa562fca4f96157
Jhonnatan Carvalho <jhonnatanwfc@live.com>
2023-12-18 13:58:35 -0300

    Adding a factory
    Factory components make sure the game objects are display properly

--- Side scroller tutorial/stars/factory.go
+++ Side scroller tutorial/stars/factory.go
@@ -32,3 +32,22 @@ embedded_components {
     w: 1.0
   }
 }
+embedded_components {
+  id: "bonus_factory"
+  type: "factory"
+  data: "prototype: \"/stars/bonus_star.go\"\n"
+  "load_dynamically: false\n"
+  "dynamic_prototype: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
